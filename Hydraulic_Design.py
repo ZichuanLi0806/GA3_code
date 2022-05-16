52e18fe6363dbb574545d89e6c6ca05a2454e49e
aw863 <aw863@cam.ac.uk>
2022-05-16 21:03:11 +0100

    Update Hydraulic_Design.py
    Now adds a corrective term in the cold side pressure loss to account for the major baffle losses

--- Hydraulic_Design.py
+++ Hydraulic_Design.py
@@ -15,32 +15,33 @@ dataset_Dph = 1e5*np.array([0.1333,0.1756,0.2024,0.2577,0.3171,0.3633,0.4233,0.4
 
 
 ###Variables for hydraulic design###
-N = 13 #Number of tubes total (not per pass)
+N = 12 #Number of tubes total (not per pass)
 N_pass_h = 1 #Number of passes of hot flow if multi-pass is used
 N_pass_c = 1 #Number of passes of cold flow if multi-pass is used
 L = 0.35 #Length of a single tube
-Y = 14e-3 #Tube pitch
+Y = 12e-3 #Tube pitch
 D_sh = 64e-3 #Acrylic shell diameter
 mu = 6.51e-4 #Dynamic viscosity (kg/ms)
 do = 8e-3 #Tube outer diameter
 di = 6e-3 #Tube inner diameter
-N_baffle = 9 #Number of baffles
-d_noz = 20e-3 #THIS NEEDS CHANGING AFTER CONSULTATION ABOUT 20MM USED INSTEAD OF 24.5????????
+N_baffle = 14 #Number of baffles PER SIDE (multipass shell)
+d_noz = 25e-3 #THIS NEEDS CHANGING AFTER CONSULTATION ABOUT 20MM USED INSTEAD OF 24.5????????
 rho = 1/0.001008 #kg/m3 (water density at 40C)
-a = 0.34 #Use 0.2 for triangular pitch. Use 0.34 for square pitch. Constant used for shell pressure drop, see handout top of page 4
-
+a = 0.2 #Use 0.20 for triangular pitch. Use 0.34 for square pitch. Constant used for shell pressure drop, see handout top of page 4
+L_plenum = 50e-3 #Plenum length
 
 
 ###Definitions of correlations###
-B = L/(N_baffle+1) #Baffle spacing
+B = L/(N_baffle+1) #Baffle spacing PER SIDE (multipass shell)
 A_sh = D_sh*(Y-do)*B/(Y*N_pass_c) #This correlation is only approx (see notes eqn 6)
 sigma = N*0.25*np.pi*di**2/(0.25*np.pi*D_sh**2) #Free area to total area. Is this equation right for multipass? i.e sigma does not vary with multipass vs signle pass?
+sigma_plenum = (np.pi*0.25*d_noz**2)/(0.5*(d_noz+D_sh)*L_plenum) #Area ratio for nozzle into plenum. Assume plenum is a rectangle area L by (d_noz+D_sh)*0.5
 
 
 ###Functions for calculations###
 def Dp_in_out(v_tube,kc,ke):
     #Returns entrance and exit pressure losses given kc and ke. These are found from edge_coefficients. Is this equation right for multipass?
-    return 0.5*rho*v_tube**2*(kc+ke)*N_pass_h    
+    return 0.5*rho*v_tube**2*(kc+ke)*N_pass_h
 def v_tube(m_dot, N):
     #Returns the tube veloctiy for the given input parameters taking into account multi-pass
     if (m_dot*N_pass_h/N)/(np.pi*rho*0.25*di**2) < 0.393:
@@ -58,12 +59,15 @@ def v_noz(m_dot,dia):
     return m_dot/(rho*0.25*np.pi*dia**2)
 def Dp_shell(V_sh,N):
     #Returns the pressure loss from the dubious empirical relation given on handout page 3 at bottom
-    return 4*a*(V_sh*do*rho/mu)**-0.15*N*rho*V_sh**2
+    return 4*a*(V_sh*do*rho/mu)**-0.15*(N/N_pass_c)*rho*V_sh**2
 def edge_coefficients(sigma):
     #Given a value of sigma, returns kc,ke from Fig 7 (assuming Re=10,000)
     kc = 0.4*(1-sigma) + 0.09 #From Fig 7. using Reynold's as approx 10,000
     ke = sigma**2 - 2.1 * sigma + 1 #From curve fit of Fig 7, again using Re=10,000
     return kc,ke
+def plenum_loss(v_noz,kc,ke):
+    #Large loss due to mixing in plenum from nozzle entry and viscous mixing. Returns pressure loss. Scaled using Longley 2022 data
+    return 9*rho*v_noz**2*(kc+ke)
 def m_dot_H(Dp_h_overall):
     #Returns hot mass flow rate for a given pressure drop along hot side. This uses Fig 6 data from handout
     a,b,c,d = np.polyfit(dataset_Dph, dataset_Qh, 3)
@@ -76,28 +80,28 @@ def m_dot_C(Dp_c_overall):
 
 
 ###Iteration to find correct mass flow rates###
-m_dot_h_actual = []
-m_dot_h_guess = []
+m_dot_h_actual = []#Returns flow rate due to guessed flow rate.
+m_dot_h_guess = [] #Guess actual flow rate
 m_dot_c_actual = []
 m_dot_c_guess = []
 
-for m_dot in np.linspace(0.15,0.7,50):
+for m_dot in np.linspace(0.15,0.7,100):
     v_t = v_tube(m_dot,N)
     f = friction_factor(v_t)
     v_noz_h = v_noz(m_dot,d_noz)
-    kc,ke = edge_coefficients(sigma)[0],edge_coefficients(sigma)[1]
-    Dp_h_overall = Dp_tubes(f,v_t) + Dp_in_out(v_t, kc, ke) + rho*v_noz_h**2 #Last term accounts for BOTH nozzles
+    kc1,ke1 = edge_coefficients(sigma)[0],edge_coefficients(sigma)[1] #For tube loss
+    kc2,ke2 = edge_coefficients(sigma_plenum)[0],edge_coefficients(sigma_plenum)[1] #For plenum loss
+    Dp_h_overall = Dp_tubes(f,v_t) + Dp_in_out(v_t, kc1, ke1) + plenum_loss(v_noz_h,kc2,ke2) + rho*v_noz_h**2 #Last term accounts for BOTH nozzles
     m_dot_h_guess.append(m_dot)
     m_dot_h_actual.append(m_dot_H(Dp_h_overall))
-    
 
 m_dot_h = intersection(m_dot_h_guess,m_dot_h_actual,m_dot_h_guess,m_dot_h_guess)[0][0] #Finds intersection of data i.e actual value of m_dot
-print("This configuration has hot mass flow:", round(m_dot_h,5),"kg/s")
+print("This configuration has hot mass flow:", round(m_dot_h,5),"kg/s (",round(m_dot_h/rho*1000,5),"ltr/s )")
 
-for m_dot in np.linspace(0.15,0.7,50):
+for m_dot in np.linspace(0.15,0.7,100):
     v_noz_c = v_noz(m_dot,d_noz)
     V_sh = m_dot/(rho*A_sh) #Measure of shell velocity
-    Dp_c_overall = Dp_shell(V_sh,N) + rho*v_noz_c**2 #Last term accounts for BOTH nozzles
+    Dp_c_overall = Dp_shell(V_sh,N) + N_baffle*N_pass_c*1.94e3 + rho*v_noz_c**2 #Last term accounts for BOTH nozzles. 2nd term is calibrated by Longley 2022 and 2017B data on Moodle
     m_dot_c_guess.append(m_dot)
     m_dot_c_actual.append(m_dot_C(Dp_c_overall))
 #plt.plot(m_dot_c_guess,m_dot_c_actual)
@@ -106,4 +110,4 @@ for m_dot in np.linspace(0.15,0.7,50):
 #plt.ylabel('Actual')
 #plt.show()
 m_dot_c = intersection(m_dot_c_guess,m_dot_c_actual,m_dot_c_guess,m_dot_c_guess)[0][0] #Finds intersection of data i.e actual value of m_dot
-print("This configuration has cold mass flow:", round(m_dot_c,5),"kg/s")
+print("This configuration has cold mass flow:", round(m_dot_c,5),"kg/s (",round(m_dot_c/rho*1000,5),"ltr/s )")
