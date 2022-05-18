5660b913a2c833b52e21a995b3c4568952d6dae0
aw863 <aw863@cam.ac.uk>
2022-05-18 10:33:58 +0100

    Refinements and added 2017 dataset
    Refinements

--- Hydraulic_Design.py
+++ Hydraulic_Design.py
@@ -12,23 +12,28 @@ dataset_Dpc = 1e5*np.array([0.1113,0.2157,0.2538,0.3168,0.3613,0.4031,0.4511,0.4
 dataset_Qh = 1e-3*np.array([0.4583,0.4236,0.4010,0.3611,0.3125,0.2639,0.2222,0.1597,0.1181,0.0694]) #m3/s
 dataset_Dph = 1e5*np.array([0.1333,0.1756,0.2024,0.2577,0.3171,0.3633,0.4233,0.4784,0.5330,0.5715]) #in Pa not bar
 
+dataset_Qc = 1e-3*np.array([0.4967,0.4739,0.4511,0.4312,0.4055,0.3856,0.3628,0.3371,0.3115,0.2887,0.2659,0.2431,0.2231,0.2003,0.1775,0.1547,0.1376,0.1120,0.0664])#2017 dataset from Moodle
+dataset_Qh = 1e-3*np.array([0.4937,0.4789,0.4640,0.4475,0.4294,0.4129,0.3980,0.3782,0.3650,0.3452,0.3320,0.3188,0.3007,0.2792,0.2644,0.2479,0.2330,0.2165,0.2000,0.1835,0.1670,0.1489,0.1340,0.1175,0.0994,0.0845,0.0664])
+dataset_Dpc = 1e5*np.array([0.0674,0.1309,0.2043,0.2654,0.3279,0.3829,0.4391,0.4903,0.5415,0.5824,0.6203,0.6541,0.6848,0.7105,0.7361,0.7577,0.7721,0.7916,0.8183])
+dataset_Dph = 1e5*np.array([0.0579,0.0845,0.1091,0.1317,0.1644,0.1890,0.2125,0.2401,0.2575,0.2891,0.3055,0.3239,0.3494,0.3750,0.3953,0.4137,0.4360,0.4564,0.4727,0.4970,0.5073,0.5276,0.5478,0.5570,0.5632,0.5734,0.5805])
+
 
 
 ###Variables for hydraulic design###
-N = 13 #Number of tubes total (not per pass)
-N_pass_h = 1 #Number of passes of hot flow if multi-pass is used
+N = 14 #Number of tubes total (not per pass)
+N_pass_h = 2 #Number of passes of hot flow if multi-pass is used
 N_pass_c = 1 #Number of passes of cold flow if multi-pass is used
-L = 0.35 #Length of a single tube
-Y = 12e-3 #Tube pitch
+L = 0.192 #Length of a single tube
+Y = 10e-3 #Tube pitch
 D_sh = 64e-3 #Acrylic shell diameter
 mu = 6.51e-4 #Dynamic viscosity (kg/ms)
 do = 8e-3 #Tube outer diameter
 di = 6e-3 #Tube inner diameter
-N_baffle = 14 #Number of baffles PER SIDE, CAREFUL. Each baffle often on both sides! (multipass shell)
+N_baffle = 8 #Number of baffles PER SIDE, CAREFUL. Each baffle often on both sides! (multipass shell)
 d_noz = 19e-3 #THIS NEEDS CHANGING AFTER CONSULTATION ABOUT 20MM USED INSTEAD OF 24.5????????
 rho = 1/0.001008 #kg/m3 (water density at 40C)
-a = 0.2 #Use 0.20 for triangular pitch. Use 0.34 for square pitch. Constant used for shell pressure drop, see handout top of page 4
-L_plenum = 50e-3 #Plenum length
+a = 0.20 #Use 0.20 for triangular pitch. Use 0.34 for square pitch. Constant used for shell pressure drop, see handout top of page 4
+L_plenum = 41e-3 #Plenum length
 
 
 ###Definitions of correlations###
@@ -99,14 +104,14 @@ for m_dot in np.linspace(0.15,0.7,100):
     m_dot_h_actual.append(m_dot_H(Dp_h_overall))
 
 m_dot_h = intersection(m_dot_h_guess,m_dot_h_actual,m_dot_h_guess,m_dot_h_guess)[0][0] #Finds intersection of data i.e actual value of m_dot
-print("This configuration has hot mass flow:", round(m_dot_h,5),"kg/s (",round(m_dot_h/rho*1000,3),"ltr/s )")
+print("HOT mass flow:", round(m_dot_h,5),"kg/s (",round(m_dot_h/rho*1000,3),"ltr/s )")
 
 for m_dot in np.linspace(0.15,0.7,100):
     v_noz_c = v_noz(m_dot,d_noz)
     V_sh = m_dot/(rho*A_sh) #Measure of shell velocity
 
     #----------------------
-    Dp_c_overall = Dp_shell(V_sh,N) + 2.3*N_baffle*N_pass_c*rho*V_sh**2 + rho*v_noz_c**2 #Last term accounts for BOTH nozzles. 2nd term is calibrated by Longley 2022 and 2017B data on Moodle
+    Dp_c_overall = Dp_shell(V_sh,N) + 20e3 + rho*v_noz_c**2 #Last term accounts for BOTH nozzles. 2nd term is calibrated by Longley 2022 and 2017B data on Moodle
     #-------------------------------
     
     
@@ -118,4 +123,4 @@ for m_dot in np.linspace(0.15,0.7,100):
 #plt.ylabel('Actual')
 #plt.show()
 m_dot_c = intersection(m_dot_c_guess,m_dot_c_actual,m_dot_c_guess,m_dot_c_guess)[0][0] #Finds intersection of data i.e actual value of m_dot
-print("This configuration has cold mass flow:", round(m_dot_c,5),"kg/s (",round(m_dot_c/rho*1000,3),"ltr/s )")
+print("COLD mass flow:", round(m_dot_c,5),"kg/s (",round(m_dot_c/rho*1000,3),"ltr/s )")
